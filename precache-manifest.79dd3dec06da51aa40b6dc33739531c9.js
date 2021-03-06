38253c22c2f085317579af8873c7a82dde844039
BryanYehuda <BryanYehuda@users.noreply.github.com>
2021-03-06 09:50:18 +0000

    Deploying to gh-pages from @ ichiro-its/shisen-rvue@ce7985c19ac0c4ae46a5d9a1c1f86fda8c1e399f 🚀

---
+++ precache-manifest.79dd3dec06da51aa40b6dc33739531c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4afeecc2740b1d16150",
+    "url": "/shisen-rvue/css/chunk-4e4cae24.e839ede7.css"
+  },
+  {
+    "revision": "eace4d80a34940e7877c",
+    "url": "/shisen-rvue/css/chunk-vendors.8c99960f.css"
+  },
+  {
+    "revision": "c02ea2af1eb9e7f77481c65c73eacafe",
+    "url": "/shisen-rvue/index.html"
+  },
+  {
+    "revision": "a43baab1feb1a8f77a17",
+    "url": "/shisen-rvue/js/app.971d6dc9.js"
+  },
+  {
+    "revision": "c4afeecc2740b1d16150",
+    "url": "/shisen-rvue/js/chunk-4e4cae24.65ff97a1.js"
+  },
+  {
+    "revision": "eace4d80a34940e7877c",
+    "url": "/shisen-rvue/js/chunk-vendors.0ef12dcd.js"
+  },
+  {
+    "revision": "c8018a1e535ee0fb024e5b983b4f4f3a",
+    "url": "/shisen-rvue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/shisen-rvue/robots.txt"
+  }
+]);
